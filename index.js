8a9ab7849d50df49ff24365a2cfe144526439f62
Zulamho <amkhadov2014@mail.ru>
2021-07-21 16:27:08 +0300

    first commit

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
-document.
-addEventListener(      'DOMContentLoaded',
-  () => {
-    const words = ['баклажан', "собака", "убийца", "гамадрил"]
-    const ranWorddddd = words[Math.floor(Math.random() * words.length)]
-    const answerArr = []
-    for (
-      let i = 0;
-      i < ranWorddddd.length;
-      i++) {answerArr.push('_')}
+document.addEventListener("DOMContentLoaded", () => {
+  const words = ["баклажан", "собака", "убийца", "гамадрил"];
+  const ranWorddddd = words[Math.floor(Math.random() * words.length)];
+  const answerArr = [];
+  for (let i = 0; i < ranWorddddd.length; i++) {
+    answerArr.push("_");
+  }
 
-    let hhhhhh = ranWorddddd.length
+  let hhhhhh = ranWorddddd.length;
 
-    while
-      (hhhhhh > 0) {
-      alert(answerArr.join(' ')
-      ); let guess = prompt('Угадайте букву'); if ( guess === null ) break
-      else
-      if (guess.length !== 1) alert('Введите только одну букву')
-
-
-
-
-      else {
-        for (let i = 0; i < ranWorddddd.length; i++) {if (ranWorddddd[i] === guess) {answerArr[i] = guess;hhhhhh-- }}
+  while (hhhhhh > 0) {
+    alert(answerArr.join(" "));
+    let guess = prompt("Угадайте букву");
+    if (guess === null) break;
+    else if (guess.length !== 1) alert("Введите только одну букву");
+    else {
+      for (let i = 0; i < ranWorddddd.length; i++) {
+        if (ranWorddddd[i] === guess) {
+          answerArr[i] = guess;
+          hhhhhh--;
+        }
       }
-
     }
+  }
 
-    alert(
-      answerArr.join(' '))
-    alert(
-
-      'Отлично, было загадано слово ' + ranWorddddd)
-  })
+  alert(answerArr.join(" "));
+  alert("Отлично, было загадано слово " + ranWorddddd);
+});
